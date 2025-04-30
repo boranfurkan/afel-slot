d3c28497e577f8643f8730800bbae5d02d55ead1
boranfurkan <furkanboran123@gmail.com>
2025-04-30 13:16:19 +0100

    feat: add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${freePixelFont.className} ${supplyMediumFont.variable} antialiased`}
+        className={`${freePixelFont.className} ${supplyMediumFont.variable} antialiased container dark`}
       >
         {children}
       </body>

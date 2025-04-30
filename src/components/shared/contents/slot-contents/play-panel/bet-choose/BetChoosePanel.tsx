b8fa815bd2c1322a2310d8c153ce46d11a42977e
boranfurkan <furkanboran123@gmail.com>
2025-04-30 13:18:23 +0100

    feat: create bet choose panel component

---
+++ src/components/shared/contents/slot-contents/play-panel/bet-choose/BetChoosePanel.tsx
@@ -0,0 +1,93 @@
+import SlotButton from '@/components/UI/SlotButton';
+import React from 'react';
+import Image from 'next/image';
+import { lamportsToSol } from '@/lib/utils';
+
+interface BetChoosePanelProps {
+  userBalance: number;
+}
+
+const BetChoosePanel = ({ userBalance }: BetChoosePanelProps) => {
+  const userBalanceInSol = parseFloat(lamportsToSol(userBalance));
+
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <SlotButton
+        className="w-full !px-3 !py-1 h-16"
+        size="large"
+        disabled={true} // Always disable BET currency buttons for now
+      >
+        <div className="flex items-center justify-center gap-2">
+          <Image
+            src="/afel-logo.png"
+            width={35}
+            height={35}
+            alt="Afel Logo"
+            className="w-8 h-8"
+          />
+          <div className="flex flex-col items-center whitespace-nowrap">
+            <span className="font-normal text-[26.99px] leading-tight tracking-[0%] align-middle">
+              0.5 BET
+            </span>
+            <span className="font-normal text-[17.54px] leading-tight tracking-[0%] align-middle">
+              BABY BET
+            </span>
+          </div>
+        </div>
+      </SlotButton>
+      <SlotButton
+        className="w-full !px-3 !py-1 h-16 flex items-center justify-center"
+        size="large"
+        disabled={userBalanceInSol < 0.1}
+      >
+        0.1 SOL
+      </SlotButton>
+      <SlotButton
+        className="w-full !px-3 !py-1 h-16 flex items-center justify-center"
+        size="large"
+        disabled={userBalanceInSol < 0.25}
+      >
+        0.25 SOL
+      </SlotButton>
+      <SlotButton
+        className="w-full !px-3 !py-1 h-16 flex items-center justify-center"
+        size="large"
+        disabled={userBalanceInSol < 0.5}
+      >
+        0.5 SOL
+      </SlotButton>
+      <SlotButton
+        className="w-full !px-3 !py-1 h-16 flex items-center justify-center"
+        size="large"
+        disabled={userBalanceInSol < 1}
+      >
+        1 SOL
+      </SlotButton>
+      <SlotButton
+        className="w-full !px-3 !py-1 h-16"
+        size="large"
+        disabled={true} // Always disable BET currency buttons for now
+      >
+        <div className="flex items-center justify-center gap-2">
+          <Image
+            src="/afel-logo.png"
+            width={35}
+            height={35}
+            alt="Afel Logo"
+            className="w-8 h-8"
+          />
+          <div className="flex flex-col items-start whitespace-nowrap">
+            <span className="font-normal text-[26.99px] leading-tight tracking-[0%] text-left">
+              2 BET
+            </span>
+            <span className="font-normal text-[17.54px] leading-tight tracking-[0%] text-left">
+              LEGEND BET
+            </span>
+          </div>
+        </div>
+      </SlotButton>
+    </div>
+  );
+};
+
+export default BetChoosePanel;

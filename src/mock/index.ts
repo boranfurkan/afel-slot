fcde5b80ae2ae022c970b699d7d7f93f9ef1a345
boranfurkan <furkanboran123@gmail.com>
2025-04-30 13:19:22 +0100

    feat: create mock data

---
+++ src/mock/index.ts
@@ -0,0 +1,6 @@
+export const MOCK_USER_DATA = {
+  walletAddress: 'H5724gTdRYhhQbRwREvdHZuRdPu1ZPtfuJZygNUhyGtQ',
+  solBalance: 1_500_000_000,
+  credit: 123_000_000_000,
+  payout: 4_052_000_000,
+};
